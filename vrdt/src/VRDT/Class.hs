d858f7c5574fed70d864f1f1f9da96c8014d30c7
James Parker <dev@jamesparker.me>
2020-01-19 21:32:07 -0500

    include state based crdt

--- vrdt/src/VRDT/Class.hs
+++ vrdt/src/VRDT/Class.hs
@@ -4,7 +4,7 @@
 
 module VRDT.Class where
 
--- | Class for verified (operation based) conflict-free replicated datatypes.
+-- | Class for (operation based) verified conflict-free replicated datatypes.
 class VRDT t where
     -- | Type that represents operations on `t`.
     type Operation t = op | op -> t
@@ -23,6 +23,28 @@ class VRDT t where
 enabled2 :: VRDT t => t -> Operation t -> Operation t -> Bool
 enabled2 x op1 op2 = enabled x op1 && enabled x op2  && enabled (apply x op1) op2 && enabled (apply x op2) op1
 
+
+
+-- | Class for (state based) verified conflict-free replicated datatypes.
+-- Must be a monotonic semilattice.
+class SVRDT t where
+    canFlowTo :: t -> t -> Bool
+    join :: t -> t -> t
+    bot  :: t
+
+    {-@ lawJoin :: z : t -> x : t -> y : t -> w : t -> {z == join x y => (canFlowTo x z && canFlowTo y z && ((canFlowTo x w && canFlowTo y w) => canFlowTo z w))} @-}
+    lawJoin :: t -> t -> t -> t -> ()
+    {-@ lawBot  :: x : t -> { canFlowTo Labels.bot x } @-}
+    lawBot  :: t -> ()
+
+    {-@ lawFlowReflexivity :: t : t -> {v : () | canFlowTo t t} @-}
+    lawFlowReflexivity :: t -> ()
+    {-@ lawFlowAntisymmetry :: a : t -> {b : t | canFlowTo a b && canFlowTo b a} -> {v : () | a == b} @-}
+    lawFlowAntisymmetry :: t -> t -> ()
+    {-@ lawFlowTransitivity :: a:t -> b:t -> c:t -> {(canFlowTo a b && canFlowTo b c) => canFlowTo a c} @-}
+    lawFlowTransitivity :: t -> t -> t -> ()
+
+    -- JP: monotonic? We should be able to derive from other laws?
 
 -- JP: CRDT monad? 
 
