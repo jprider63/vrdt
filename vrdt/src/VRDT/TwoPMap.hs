550a61361ba9f639ba0f6d274dae483b17513215
James Parker <dev@jamesparker.me>
2020-01-25 12:54:27 -0600

    2PMap that doesn't depend on causal order

--- vrdt/src/VRDT/TwoPMap.hs
+++ vrdt/src/VRDT/TwoPMap.hs
@@ -4,12 +4,11 @@ module VRDT.TwoPMap where
 
 import Data.Map (Map)
 import qualified Data.Map as Map
+import Data.Maybe
 import Data.Set (Set)
 import qualified Data.Set as Set
 
 import VRDT.Class
-
--- TODO: LWWMap Map k (t,v)
 
 
 -- Keys are (ClientId, NextId)?
@@ -17,8 +16,10 @@ import VRDT.Class
 -- Two phase map (inserted and deleted).
 data TwoPMap k v = TwoPMap {
     twoPMap :: Map.Map k v
+  , twoPMapPending :: Map k [Operation v] -- ^ Pending operations of v (which hasn't been inserted yet).
   , twoPMapTombstone :: Set k
   }
+-- TODO: Invariant that keys are disjoint.
 
 data TwoPMapOp k v = 
     TwoPMapInsert k v
@@ -28,23 +29,34 @@ data TwoPMapOp k v =
 instance (VRDT v, Ord k) => VRDT (TwoPMap k v) where
     type Operation (TwoPMap k v) = TwoPMapOp k v
 
-    enabled (TwoPMap m t) (TwoPMapInsert k _v) = not $ Map.member k m || Set.member k t
-    enabled (TwoPMap m t) (TwoPMapApply k _op) = Map.member k m || Set.member k t -- JP: This requires causal causal consistency. Can't apply an operation until it's inserted. 
-    enabled (TwoPMap m t) (TwoPMapDelete k) = True
+    enabled (TwoPMap m _p t) (TwoPMapInsert k _v) = not $ Map.member k m || Set.member k t
+    enabled (TwoPMap m _p t) (TwoPMapApply k _op) = True
+    enabled (TwoPMap m _p t) (TwoPMapDelete k) = True
     
-    apply (TwoPMap m t) (TwoPMapInsert k v) = 
+    apply (TwoPMap m p t) (TwoPMapInsert k v) = 
+        -- Check if deleted.
         if Set.member k t then
-            TwoPMap m t
+            TwoPMap m p t
         else
-            let m' = Map.insert k v m in
-            TwoPMap m' t
-    apply (TwoPMap m t) (TwoPMapApply k op) = 
-        let m' = Map.adjust (\v -> apply v op) k m in
-        TwoPMap m' t
-    apply (TwoPMap m t) (TwoPMapDelete k) =
+            -- Apply pending operations.
+            let (opsM, p') = Map.updateLookupWithKey (const $ const Nothing) k p in
+            let v' = maybe v (foldr (\op v -> apply v op) v) opsM in -- $ Map.lookup k p in
+            -- let p' = Map.delete k p in
+
+            let m' = Map.insert k v' m in
+            TwoPMap m' p' t
+    apply (TwoPMap m p t) (TwoPMapApply k op) = 
+        let (updatedM, m') = Map.updateLookupWithKey (\_ v -> Just $ apply v op) k m in
+        
+        -- Add to pending if not inserted.
+        let p' = if isJust updatedM then p else Map.insertWith (++) k [op] p in
+
+        TwoPMap m' p' t
+    apply (TwoPMap m p t) (TwoPMapDelete k) =
         let m' = Map.delete k m in
+        let p' = Map.delete k p in
         let t' = Set.insert k t in
-        TwoPMap m' t'
+        TwoPMap m' p' t'
 
     lawCommutativity _ _ _ = ()
     
