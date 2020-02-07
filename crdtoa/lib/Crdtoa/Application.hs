fd5a4155425cbadea65669b3a939a82b63e5a9a2
PLR <51248199+plredmond@users.noreply.github.com>
2020-02-06 23:11:20 -0800

    implement a bidirectional-stream approach; it does not work! 1) server gives 415 and 2) client cannot resend messages

--- crdtoa/lib/Crdtoa/Application.hs
+++ crdtoa/lib/Crdtoa/Application.hs
@@ -10,19 +10,18 @@ module Crdtoa.Application
 , Ser.Serialize
 ) where
 
-import Control.Monad (when, forever, (>=>))
+import Control.Monad ((>=>))
 import Servant (Proxy(..), (:<|>)(..), NoContent(..))
-import qualified Control.Concurrent as Conc
-import qualified Servant as Servant hiding (Proxy, (:<|>))
 import qualified Control.Concurrent.Async as Async
 import qualified Control.Concurrent.STM as STM
 import qualified Control.Exception as Exc
 import qualified Data.Serialize as Ser
+import qualified Data.UUID.V4 as UUIDv4
 import qualified Network.HTTP.Client as HTTP
 import qualified Network.HTTP.Client.TLS as TLS
+import qualified Servant as Servant hiding (Proxy, (:<|>))
 import qualified Servant.Client.Streaming as Client
 import qualified Servant.Types.SourceT as SourceT
-import qualified Data.UUID.V4 as UUIDv4
 
 import Servant.Extras ()
 import qualified Control.Concurrent.STM.Extras as STME
@@ -34,12 +33,10 @@ maxBackoffSec = 600 -- five minutes
 -- FIXME: when connecting to an empty store, no data is sent to the client and
 -- so the client reaches ResponseTimeout
 --
--- FIXME: when the sender becomes active, the listener should become active too
---      TODO: pull the wakeup tmvar out as an abstraction
---
 -- TODO: log to stderr; look up logging libraries; monad-logger maybe?
 --
--- TODO: generate a clientid with uuid:Data.UUID.V4.nextRandom
+-- TODO: make a pipes or conduit based interface? what about other
+-- serialization libraries?
 
 createV0 :: Client.ClientM API.StoreId
 sendV0 :: API.StoreId -> API.AppData -> Client.ClientM NoContent
@@ -59,13 +56,19 @@ data Client a = Client
     , send :: a -> IO ()
     }
 
-runRaw'
+-- | A callback-based interface for an application which sends and receives
+-- 'API.AppData' values.
+--
+-- Prefer one of the @with*@ variants such as 'withSer' or 'withRaw' over use
+-- of this function. If you do use this function, you'll need to clean up the
+-- background resources yourself.
+runRaw
     :: Server
     -> Maybe API.StoreId
     -> Maybe API.ClientId
     -> Recv API.AppData
     -> IO (Client API.AppData)
-runRaw' (Server server) storeSpec clientSpec recv = do
+runRaw (Server server) storeSpec clientSpec recv = do
     env <- Client.mkClientEnv
         <$> HTTP.newManager TLS.tlsManagerSettings
         <*> Client.parseBaseUrl server
@@ -73,7 +76,19 @@ runRaw' (Server server) storeSpec clientSpec recv = do
     client <- maybe createClient return clientSpec
     outbox <- STM.newTQueueIO
     pillow <- STME.newWakeupIO
-    background <- Async.async $ beManager env store client outbox pillow
+    -- The background thread runs the connect-loop once. It will complain if an
+    -- exception occurs or the connect-loop finishes early. An additional
+    -- exception will be raised when waiting on the async at application quit.
+    background <- Async.async $ let
+        connect = connectStream env store client (sendFrom outbox) (receiveTo recv)
+        action = do
+            connectLoop connect pillow 0
+            fail "background thread ended early"
+        handlers =
+            [ Exc.Handler ignoreAsyncCancelled -- Don't complain when exiting
+            , Exc.Handler $ complainAndRethrowSomeException "background,catches"
+            ]
+        in action `Exc.catches` handlers
     return Client
         { background = background
         , store = store
@@ -87,22 +102,19 @@ runRaw' (Server server) storeSpec clientSpec recv = do
     createStore env
         = Client.withClientM createV0 env
         $ either (error "createStore,either") return
-    -- The manager runs the sender and listener once. It will complain if
-    -- either of them quits or throws an exception. An additional exception
-    -- will be raised when the application quits.
-    beManager env store client outbox pillow
-        = connectLoop (justConnect env store client (send outbox) (receive recv)) pillow 0
-        `Exc.catches`
-        [ Exc.Handler ignoreAsyncCancelled -- Don't complain when exiting
-        , Exc.Handler $ complainAndRethrowSomeException "beManager (stream)"
-        ]
-    send outbox = Servant.toSourceIO outbox
+        -- TODO this junk ^ eats actual errors
 
-receive :: Recv API.AppData -> API.DownStream -> IO ()
-receive (Recv recv) = 
-    (SourceT.foreach complainMidstream recv . SourceT.mapMaybe justUpdates)
+sendFrom :: STM.TQueue API.AppData -> API.UpStream
+sendFrom = Servant.toSourceIO
+-- FIXME: when sending an update fails, resulting in a disconnect, this loses
+-- the update completely.. there's no backpressure so we can't fix that
+
+receiveTo :: Recv API.AppData -> API.DownStream -> IO ()
+receiveTo (Recv recv)
+    = SourceT.foreach complainMidstream recv
+    . SourceT.mapMaybe justUpdates
   where
-    -- Filter to only Update messages until we can cache locally and resend.
+    -- Filter to only Update messages until TODO caching locally and resend.
     justUpdates = \case
         API.Update _ update -> Just update
         API.RequestResendUpdates -> Nothing
@@ -115,178 +127,82 @@ receive (Recv recv) =
 -- the other end which responds positively to the request we sent.
 data ConnectionAttempt = Ok | Demerit
 
-justConnect :: Client.ClientEnv -> API.StoreId -> API.ClientId -> API.UpStream -> (API.DownStream -> IO ()) -> IO ConnectionAttempt
-justConnect env store client source sink = action `Exc.catches` handlers
+-- | Backoff before running the next connection-attempt according to the number
+-- of demerits since the last successful connection.
+connectLoop :: IO ConnectionAttempt -> STME.Wakeup -> Int -> IO ()
+connectLoop tryConnect wakeup demerits = do
+    exponentialBackoff wakeup demerits
+    tryConnect >>= adjustDemerits >>= connectLoop tryConnect wakeup
+  where
+    adjustDemerits = return . \case
+        Demerit -> 1 + demerits
+        Ok -> 0
+
+-- | Make one connection attempt and block while sending from the up-stream and
+-- receiving the down-stream. Return whether or not the attempt worked.
+connectStream :: Client.ClientEnv -> API.StoreId -> API.ClientId -> API.UpStream -> (API.DownStream -> IO ()) -> IO ConnectionAttempt
+connectStream env store client source sink
+    = action `Exc.catches` handlers
   where
     constM = const . return
     action
         = Client.withClientM (streamV0 store client source) env
         $ either
-            (complainClientError "connect,either" >=> constM Demerit)
+            (complainClientError "connectStream,either" >=> constM Demerit)
             (sink >=> constM Ok)
-    handlers=
-        [ Exc.Handler $ complainClientError "listener,catches" >=> constM Demerit
+    handlers =
+        [ Exc.Handler $ complainClientError "connectStream,catches" >=> constM Demerit
         , Exc.Handler $ \case
             HTTP.HttpExceptionRequest _ HTTP.IncompleteHeaders ->
                 putStrLn "Disconnect midstream. Reconnecting.." >> return Ok
             HTTP.HttpExceptionRequest _ HTTP.NoResponseDataReceived ->
                 putStrLn "Disconnect before stream. Reconnecting.." >> return Ok
             exc ->
-                complainHttpException "listener,catches" exc >> return Demerit
+                complainHttpException "connectStream,catches" exc >> return Demerit
         ]
 
-connectLoop :: IO ConnectionAttempt -> STME.Wakeup -> Float -> IO ()
-connectLoop action wakeup demerits = do
-    backoff wakeup demerits
-    action >>= \case
-        Ok -> connectLoop action wakeup 0
-        Demerit -> connectLoop action wakeup $ 1 + demerits
-
-backoff :: STME.Wakeup -> Float -> IO ()
-backoff wakeup demerits = do
+-- | Sleep exponentially according to the number of demerits.
+--
+-- FIXME: this function isn't necessary once we have a proper logging library?
+exponentialBackoff :: STME.Wakeup -> Int -> IO ()
+exponentialBackoff wakeup demerits = do
     putStrLn $ "[DEBUG] Demerits:" <> show demerits <> ", BackoffSec:" <> show backoffSec
     STME.sleepSec wakeup $ fromIntegral backoffSec
   where
-    backoffSec = exponentialBackoffSec maxBackoffSec demerits
-
--- | A callback-based interface for an application which sends and receives
--- 'API.AppData' values to the store via the server.
---
--- Prefer one of the @with*@ variants such as 'withSer' or 'withRaw' over use
--- of this function. If you do use this function, you'll need to clean up the
--- background resources yourself.
-runRaw
-    :: Server
-    -> Maybe API.StoreId
-    -> Recv API.AppData
-    -> IO (Client API.AppData)
-runRaw (Server server) requestStore (Recv recv) = do
-    env <- Client.mkClientEnv
-        <$> HTTP.newManager TLS.tlsManagerSettings
-        <*> Client.parseBaseUrl server
-
-    let Just store = requestStore -- FIXME: call createV0
-
-    outbox <- STM.atomically $ STM.newTQueue
-    wakeup <- STM.atomically $ STM.newEmptyTMVar
-    background <- Async.async $ beManager env store outbox wakeup
-    return Client
-        { background = background
-        , store = store
-        , send = \a -> STM.atomically $ do
-            _ <- STM.tryPutTMVar wakeup ()
-            STM.writeTQueue outbox a
-        }
-  where
-    -- The manager runs the sender and listener once. It will complain if
-    -- either of them quits or throws an exception. An additional exception
-    -- will be raised when the application quits.
-    beManager env store outbox wakeup
-        = action `Exc.catches` handlers
-      where
-        action = do
-            Async.withAsync (beSender env store outbox wakeup) $ \sender -> do
-                Async.withAsync (beListener env store wakeup 0) $ \listener -> do
-                    (a, _) <- Async.waitAny [sender, listener]
-                    case a of
-                        _ | a == sender -> fail "sender thread exited early"
-                          | a == listener -> fail "sender thread exited early"
-                          | otherwise -> fail "an unknown thread exited early"
-        handlers =
-            [ Exc.Handler ignoreAsyncCancelled -- Don't complain when exiting
-            , Exc.Handler $ complainAndRethrowSomeException "manager"
-            ]
-    -- The sender blocks on outbox and sends things forever, until there's an
-    -- error. Then it blocks on wakeup until either the listener or a send
-    -- action wakes it up.
-    beSender env store outbox wakeup = forever $ do
-        putStrLn "[DEBUG] Sender waiting for item"
-        item <- STM.atomically $ do
-            _ <- STM.tryTakeTMVar wakeup
-            STM.readTQueue outbox
-        putStrLn "[DEBUG] Sender working"
-        sentItem <- (action item `Exc.catches` handlers)
-        when (not sentItem) $ do
-            putStrLn "[DEBUG] Sender going to sleep"
-            STM.atomically $ do
-                STM.unGetTQueue outbox item
-                STM.takeTMVar wakeup
-      where
-        action item =
-            Client.runClientM (sendV0 store item) env
-                >>= either
-                    (complainClientError "sender,either" >=> unsent)
-                    (acceptNoContent >=> sent)
-                    -- TODO wakeup listener on success
-        handlers =
-            [ -- TODO: fill in handlers when we see exceptions reach the manager
-            ]
-        sent = const . return $ True
-        unsent = const . return $ False
-    -- The listener attempts to listen forever, handling its own error cases
-    -- internally. It counts how many errors since it last heard back from the
-    -- server, and backs off.
-    beListener env store wakeupSender errors = do
-        putStrLn $ "[DEBUG] Errors:" <> show errors <> ", BackoffSec:" <> show (exponentialBackoffSec maxBackoffSec errors)
-        exponentialBackoff maxBackoffSec errors
-        (action `Exc.catches` handlers)
-            >>= beListener env store wakeupSender
-      where
-        action =
-            Client.withClientM (listenV0 store) env
-            . either (complainClientError "listener,either" >=> demerit)
-            $ \stream -> do
-                _ <- STM.atomically $ STM.tryPutTMVar wakeupSender ()
-                SourceT.foreach complainMidstream recv stream
-                reset ()
-        handlers =
-            [ Exc.Handler $ complainClientError "listener,catches" >=> demerit
-            , Exc.Handler $ \case
-                HTTP.HttpExceptionRequest _ HTTP.IncompleteHeaders ->
-                    putStrLn "Disconnect midstream. Reconnecting.." >> reset ()
-                HTTP.HttpExceptionRequest _ HTTP.NoResponseDataReceived ->
-                    putStrLn "Disconnect before stream. Reconnecting.." >> reset ()
-                exc ->
-                    complainHttpException "listener,catches" exc >> demerit ()
-            ]
-        -- reset and demerit are shorthand for adjusting the error count which
-        -- is used to compute backoff.
-        demerit = const . return $ errors + 1
-        reset = const . return $ 1
-        -- This handles an error sent intentionally by the stream producer
-        -- instead of treating it like a generic exception.
-        complainMidstream err = putStrLn $ "[ERROR] Received from stream: " <> err
+    backoffSec = exponentialBackoffSec maxBackoffSec $ fromIntegral demerits
 
 -- | A callback-based interface for an application which sends and receives
 -- 'API.AppData' values.
 --
--- Connect with the server to exchange updates in the store. If no store is
--- specified, the server will generate one. It is available in the returned
--- 'Client'.
--- 
 -- This function follows the bracket-pattern to ensure that internal resources
--- are cleaned up.  If an exception occurred in a background thread, it will be
+-- are cleaned up. If an exception occurred in a background thread, it will be
 -- re-raised when this returns.
 withRaw
     :: Server
     -> Maybe API.StoreId
+    -> Maybe API.ClientId
     -> Recv API.AppData
     -> (Client API.AppData -> IO a)
     -> IO a
-withRaw server store recv = Exc.bracket acquire release
+withRaw server store client recv
+    = Exc.bracket acquire release
   where
-    acquire = runRaw server store recv
+    acquire = runRaw server store client recv
     release Client{background} = do
         Async.cancel background
         Async.wait background `Exc.catch` ignoreAsyncCancelled -- Don't complain when exiting
 
 -- | A callback-based interface for an application which sends and receives
--- 'Ser.Serialize'able values to the store via the server and follows the
--- bracket-pattern to ensure that internal resources are cleaned up.
+-- 'Ser.Serialize'able values.
+--
+-- This function follows the bracket-pattern to ensure that internal resources
+-- are cleaned up. If an exception occurred in a background thread, it will be
+-- re-raised when this returns.
 withSer
     :: Ser.Serialize u
     => Server
     -> Maybe API.StoreId
+    -> Maybe API.ClientId
     -> Recv (Either String u)
     -- ^ Deserialization errors are exposed to the receive function so that the
     -- application can be made aware of error cases, eg. receiving data from an
@@ -295,13 +211,11 @@ withSer
     -> (Client u -> IO a)
     -- ^ The send function will serialize data before writing to the wire.
     -> IO a
-withSer server store (Recv recvSer) actionSer = withRaw server store (Recv recvRaw) actionRaw
+withSer server store client (Recv recvSer) actionSer
+    = withRaw server store client (Recv recvRaw) actionRaw
   where
     recvRaw = recvSer . Ser.decodeLazy . \(API.AppData bs) -> bs
-    actionRaw client@Client{send=sendRaw} = actionSer client{send=sendRaw . API.AppData . Ser.encodeLazy}
-
--- TODO: make a pipes or conduit based interface? what about other
--- serialization libraries?
+    actionRaw c@Client{send=sendRaw} = actionSer c{send=sendRaw . API.AppData . Ser.encodeLazy}
 
 -- FIXME: on the first connection to a new store, the listen times out
 -- because the server never sends anything back.. debugging with curl, we
@@ -343,12 +257,6 @@ withSer server store (Recv recvSer) actionSer = withRaw server store (Recv recvR
 -- [0,2,6,19,54,147,402,600,600,600,600]
 exponentialBackoffSec :: Int -> Float -> Int
 exponentialBackoffSec cap = min (abs cap) . subtract 1 . round . exp . abs
-
-exponentialBackoff :: Int -> Float -> IO ()
-exponentialBackoff cap = Conc.threadDelay . secToMicrosec . exponentialBackoffSec cap
-  where
-    round' = round :: Float -> Int
-    secToMicrosec = (* round' 1e6)
 
 -- | Log a message about an 'HTTP.HttpExceptionRequest' exception.
 --
