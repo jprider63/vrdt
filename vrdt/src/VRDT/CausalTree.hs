6f6a5806bb8de26e8cfb86eaa4441cf357c84286
James Parker <dev@jamesparker.me>
2020-03-05 12:31:19 -0500

    preorder of causal tree

--- vrdt/src/VRDT/CausalTree.hs
+++ vrdt/src/VRDT/CausalTree.hs
@@ -13,30 +13,35 @@ data CausalTree a = CausalTree {
   -- , CausalTreeList :: [a] -- JP: Should we add this field to cache the list representation?
   , causalTreePending :: Map AtomId [CausalTreeAtom a]
   }
+  deriving (Show)
 
 data CausalTreeOp a = CausalTreeOp {
     causalTreeOpParent :: AtomId
   , causalTreeOpAtom   :: CausalTreeAtom a    -- Invariant: Cannot be CausalTreeLetterRoot
   }
+  deriving (Show)
 
 -- JP: Maybe we should leave this abstract.
 data AtomId = AtomId UTCTime ClientId
-    deriving (Eq, Ord)
+  deriving (Show, Eq, Ord)
 
 data CausalTreeAtom a = CausalTreeAtom {
     causalTreeAtomId     :: AtomId
   , causalTreeAtomLetter :: CausalTreeLetter a
   }
+  deriving (Show)
 
 data CausalTreeLetter a = 
     CausalTreeLetter a
   | CausalTreeLetterDelete
   | CausalTreeLetterRoot -- | Root node. Should only be used as the initial node on creation.
+  deriving (Show)
 
 data CausalTreeWeave a = CausalTreeWeave {
     causalTreeWeaveAtom     :: CausalTreeAtom a
   , causalTreeWeaveChildren :: [CausalTreeWeave a]
   }
+  deriving (Show)
 
 apply :: CausalTree a -> CausalTreeOp a -> CausalTree a
 apply ct (CausalTreeOp parentId atom) = applyAtom ct parentId atom
@@ -84,12 +89,44 @@ apply ct (CausalTreeOp parentId atom) = applyAtom ct parentId atom
       | otherwise                                    = w:insertAtom ws atom
 
 
+-- | Preorder traversal of a `CausalTree`. Returns atoms in reverse order.
+-- Invariant: `CausalTreeLetter` is the only constructor in the returned list.
+-- O(n)
+--
+-- JP: Can we efficiently return results in the correct order by doing a foldr?
+preorder :: CausalTree a -> [CausalTreeAtom a]
+preorder = go [] . causalTreeWeave
+ where
+  go acc (CausalTreeWeave _atom ws) = 
+    let (_, acc') = List.foldl' visitor (False, acc) ws in
+    List.foldl' go acc' ws 
+
+  visitor (deleted, acc) (CausalTreeWeave atom _ws) = case causalTreeAtomLetter atom of
+    CausalTreeLetterDelete ->
+      -- Skip if already deleted.
+      if deleted then
+        (deleted, acc)
+      else
+        (True, pop acc)
+    
+    CausalTreeLetterRoot ->
+      -- Should be unreachable.
+      (deleted, acc)
+
+    CausalTreeLetter _ ->
+      (deleted, atom:acc)
+  
+  pop = List.drop 1
+      
+
+
 -- Compare whether an atom is greater than another atom, prioritizing CausalTreeLetterDelete.
 atomGreaterThan :: CausalTreeAtom a -> CausalTreeAtom a -> Bool
-atomGreaterThan (CausalTreeAtom a1 CausalTreeLetterRoot) (CausalTreeAtom a2 CausalTreeLetterRoot) = a1 > a2
-atomGreaterThan (CausalTreeAtom _ CausalTreeLetterRoot) (CausalTreeAtom _ _) = True
+atomGreaterThan (CausalTreeAtom a1 CausalTreeLetterRoot) (CausalTreeAtom a2 CausalTreeLetterRoot)     = a1 > a2
+atomGreaterThan (CausalTreeAtom _ CausalTreeLetterRoot) (CausalTreeAtom _ _)                          = True
 atomGreaterThan (CausalTreeAtom a1 CausalTreeLetterDelete) (CausalTreeAtom a2 CausalTreeLetterDelete) = a1 > a2
-atomGreaterThan (CausalTreeAtom _ CausalTreeLetterDelete) (CausalTreeAtom _ _) = True
-atomGreaterThan (CausalTreeAtom a1 (CausalTreeLetter _)) (CausalTreeAtom a2 (CausalTreeLetter _)) = a1 > a2
+atomGreaterThan (CausalTreeAtom _ CausalTreeLetterDelete) (CausalTreeAtom _ _)                        = True
+atomGreaterThan (CausalTreeAtom a1 (CausalTreeLetter _)) (CausalTreeAtom a2 (CausalTreeLetter _))     = a1 > a2
+atomGreaterThan (CausalTreeAtom a1 (CausalTreeLetter _)) (CausalTreeAtom a2 _)                        = False
 
 
--- vrdt/src/VRDT/Types.hs
+++ vrdt/src/VRDT/Types.hs
@@ -2,3 +2,27 @@
 module VRDT.Types where
 
 -- TODO: Delete this module?
+-- import           Data.Aeson (FromJSON(..), ToJSON(..), (.=), (.:))
+-- import qualified Data.Aeson as Aeson
+-- import           Data.ByteString (ByteString)
+-- import qualified Data.ByteString as BS
+-- import qualified Data.ByteString.Base64.URL as B64U
+-- import qualified Data.Text.Encoding as Text
+-- import           Data.Time.Clock (UTCTime)
+-- 
+-- 
+-- {-@
+-- data ClientId = ClientId {
+--     rawClientId :: {v : ByteString | BS.length v == 16}
+--   }
+-- @-}
+-- data ClientId = ClientId {
+--     rawClientId :: ByteString -- 128 bits
+--   }
+--   deriving (Show, Eq, Ord)
+-- 
+-- instance FromJSON ClientId where
+--     parseJSON = Aeson.withText "ClientId" $ either fail (return . ClientId) . B64U.decode . Text.encodeUtf8 
+-- instance ToJSON ClientId where
+--     toJSON (ClientId c) = toJSON $ Text.decodeUtf8 $ B64U.encode c
+
