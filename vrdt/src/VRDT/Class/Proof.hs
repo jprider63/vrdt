66ae6f6ef989e6eaf2ce939cf0304ac29d2998ba
James Parker <dev@jamesparker.me>
2020-03-24 12:00:39 -0400

    sec proof statement

---
+++ vrdt/src/VRDT/Class/Proof.hs
@@ -0,0 +1,41 @@
+{-@ LIQUID "--reflection" @-}
+{-@ LIQUID "--ple" @-}
+
+module VRDT.Class.Proof where
+
+import VRDT.Class
+
+
+-- JP: This definition differs slightly from Shapiro's since order doesn't matter for us.
+{-@ strongEventualConsistency :: (Eq (Operation a), VRDT a) => s0 : a -> {ops1 : [Operation a] | allEnabled s0 ops1} -> {ops2 : [Operation a] | allEnabled s0 ops2} -> {isPermutation ops1 ops2 => applyAll s0 ops1 = applyAll s0 ops2} @-}
+strongEventualConsistency :: VRDT a => a -> [Operation a] -> [Operation a] -> ()
+strongEventualConsistency _ _ _ = ()
+
+{-@ reflect allEnabled @-}
+allEnabled :: VRDT a => a -> [Operation a] -> Bool
+allEnabled _  []       = True
+allEnabled s0 (op:ops) = enabled s0 op && allEnabled (apply s0 op) ops
+
+{-@ reflect applyAll @-}
+applyAll :: VRDT a => a -> [Operation a] -> a
+applyAll s []       = s
+applyAll s (op:ops) = applyAll (apply s op) ops
+
+{-@ reflect isPermutation @-}
+isPermutation :: Eq o => [o] -> [o] -> Bool
+isPermutation []    []    = True
+isPermutation (_:_) []    = False
+isPermutation []    (_:_) = False
+isPermutation (op1:ops1') ops2 = case removeFirst op1 ops2 of
+    Nothing -> False
+    Just ops2' -> isPermutation ops1' ops2'
+
+{-@ reflect removeFirst @-}
+removeFirst :: Eq o => o -> [o] -> Maybe [o]
+removeFirst o [] = Nothing
+removeFirst o (h:t) = 
+  if h == o then 
+    Just t 
+  else 
+    (h:) <$> removeFirst o t
+
