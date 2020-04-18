ff6581ae217b05de9d0b07f1f7db928f6bddd63c
James Parker <dev@jamesparker.me>
2020-04-18 03:52:19 -0400

    make Liquid maps sorted

--- vrdt/src/Liquid/Data/Map.hs
+++ vrdt/src/Liquid/Data/Map.hs
@@ -1,13 +1,30 @@
 {-@ LIQUID "--reflection" @-}
+{-@ LIQUID "--ple-local" @-}
 
 module Liquid.Data.Map where 
 
 import Prelude hiding (Maybe(..), lookup)
 import Liquid.Data.Maybe 
+import Liquid.ProofCombinators
 import qualified Data.Set as S
 
-data Map k v = Tip | Map k v (Map k v)
-{-@ data Map k v = Tip | Map {k::k,  v::v, m::Map {kk:k | kk /= k} v} @-}
+data Map k v = 
+    Tip 
+  | Map {
+      mapKey :: k
+    , mapValue :: v 
+    , mapTail :: Map k v
+    }
+{-@ 
+data Map k v = Tip
+  | Map {
+      mapKey :: k
+    , mapValue :: v 
+    , mapTail :: Map {kk: k | mapKey < kk} v
+    }
+@-}
+-- data Map k v = Tip | Map {k::k,  v::v, m::Map {kk:k | k < kk} v}
+-- {-@ data Map k v = Tip | Map {k::k,  v::v, m::Map {kk:k | k /= kk} v} @-}
 
 
 
@@ -49,16 +66,49 @@ size (Map _ _ m) = 1 + size m
 insert :: Ord k => k -> v -> Map k v -> Map k v
 insert k v Tip = Map k v Tip
 insert k' v' (Map k v m)
-  | k == k'   = Map k v' m
+  | k' == k   = Map k v' m
+  | k' < k    = Map k' v' (Map k v m) -- ? assume ()
   | otherwise = Map k v (insert k' v' m)
 
+-- {-@ 
+-- assume keyEqLemma 
+--   :: Ord k 
+--   => k':k 
+--   -> k:k 
+--   -> v:v 
+--   -> {t:Map k v | k == mapKey (Map k v t)}
+--   -> {k' >= k => not (S.member k' (keys t))}
+-- @-}
+-- keyEqLemma 
+--   :: Ord k 
+--   => k 
+--   -> k 
+--   -> v 
+--   -> Map k v
+--   -> ()
+-- keyEqLemma k' k v t = () -- undefined -- TODO
+
+-- keyEqLemma k' k v Tip = () -- undefined -- TODO
+-- keyEqLemma k' _ _ (Map k v t) = case t of
+--   Tip -> ()
+--   Map k2 v2 t2 -> keyEqLemma k' k2 v2 t2
+
+-- {-@ ple delete @-}
 {-@ reflect delete @-}
 {-@ delete :: Ord k => k:k -> m:Map k v -> {v:Map k v | if (S.member k (keys m)) then (keys v == S.difference (keys m) (S.singleton k)) else (keys m == keys v) } @-}
 delete :: Ord k => k -> Map k v -> Map k v 
 delete _ Tip  = Tip 
 delete k' (Map k v m)
-  | k == k'   = delete k' m 
+  | k' == k   = delete k' m
+  -- | k' == k   = m ? keyEqLemma k' k v m
+
+  | k' > k    = Map k v (delete k' m)
+  -- | k' > k    = Map k v m ? keyEqLemma k' k v m
+
+
+  -- k' < k
   | otherwise = Map k v (delete k' m)
+
 
 {-@ reflect lookup @-}
 lookup :: Ord k => k -> Map k v -> Maybe v 
