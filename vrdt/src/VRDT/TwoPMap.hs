3dfb4c1ab420478d89500aec2a37730305d3cec8
Yiyun Liu <liuyiyun@terpmail.umd.edu>
2020-05-14 19:09:45 -0400

    2pmap +1 case

--- vrdt/src/VRDT/TwoPMap.hs
+++ vrdt/src/VRDT/TwoPMap.hs
@@ -217,10 +217,31 @@ lawCommutativity :: (Ord k, Ord (Operation v), VRDT v) => TwoPMap k v -> TwoPMap
 -- lawCommutativity x@(TwoPMap m p t) op2 (TwoPMapApply k op)
 --   | Set.member k t
 --   = Set.member k (twoPMapTombstone (applyTwoPMap x op2)) `cast` ()
-lawCommutativity (TwoPMap m p t) (TwoPMapApply k op) (TwoPMapApply k' op')
+
+-- apply/apply, k == k'
+
+-- lawCommutativity (TwoPMap m p t) (TwoPMapApply k op) (TwoPMapApply k' op')
+--   | not (Set.member k t)
+--   , not (Set.member k' t)
+--   , k == k'
+--   , not (compatible op op')
+--   = ()
+lawCommutativity x@(TwoPMap m p t) op1@(TwoPMapApply k vop) op2@(TwoPMapApply k' vop')
   | not (Set.member k t)
   , not (Set.member k' t)
-  = ()
+  , k == k'
+  , compatibleTwoPMap op1 op2
+  , Just vv <- Map.lookup k m
+  = let vv_vop = apply vv vop
+        vv_vop' = apply vv vop' in
+      VRDT.Class.lawCommutativity vv vop vop' `cast`
+      lemmaInsertTwice k (apply vv_vop vop') vv_vop m `cast`
+      lemmaInsertTwice k (apply vv_vop' vop) vv_vop' m `cast`
+      lemmaLookupInsert m k vv_vop `cast`
+      lemmaLookupInsert m k vv_vop' `cast`
+      ()
+
+        
 lawCommutativity _ _ _
   = ()
 
