e46b469914f540704000722b6655f7cfc913b585
Yiyun Liu <liuyiyun@terpmail.umd.edu>
2020-05-22 22:41:43 -0400

    pull out lemma for AA

--- vrdt/src/VRDT/TwoPMap/LemmaAAEq.hs
+++ vrdt/src/VRDT/TwoPMap/LemmaAAEq.hs
@@ -18,6 +18,10 @@ import           Prelude hiding (Maybe(..), isJust, maybe, foldr, flip, const)
 import           Liquid.Data.Maybe
 import           VRDT.Class.Proof
 
+{-@ lemmaAllCompatibleInsert :: (Ord (Operation a), VRDT a) => ops:[Operation a] -> v0:Operation a -> v1:Operation a -> {(allCompatible' v0 ops && allCompatible' v1 ops && compatible v0 v1) => allCompatible' v0 (insertList v1 ops)} @-}
+lemmaAllCompatibleInsert :: (Ord (Operation a), VRDT a) => [Operation a] -> Operation a -> Operation a -> ()
+lemmaAllCompatibleInsert = undefined
+
 {-@ lawCommutativityAAEq :: (Ord k, Ord (Operation v), VRDT v) => x : TwoPMap k v -> k1:k -> vop1:Operation v -> vop2:Operation v -> {(compatibleTwoPMap (TwoPMapApply k1 vop1) (TwoPMapApply k1 vop2) && compatibleStateTwoPMap x (TwoPMapApply k1 vop1) && compatibleStateTwoPMap x (TwoPMapApply k1 vop2))  => ((applyTwoPMap (applyTwoPMap x (TwoPMapApply k1 vop1)) (TwoPMapApply k1 vop2) == applyTwoPMap (applyTwoPMap x (TwoPMapApply k1 vop2)) (TwoPMapApply k1 vop1)) && compatibleStateTwoPMap (applyTwoPMap x (TwoPMapApply k1 vop1)) (TwoPMapApply k1 vop2))} @-}
 lawCommutativityAAEq :: (Ord k, Ord (Operation v), VRDT v) => TwoPMap k v -> k -> Operation v -> Operation v -> ()
 lawCommutativityAAEq x@(TwoPMap m p t) k vop vop'
@@ -36,9 +40,33 @@ lawCommutativityAAEq x@(TwoPMap m p t) k vop vop'
       lemmaLookupInsert m k vv_vop &&&
       lemmaLookupInsert m k vv_vop' &&&
       ()
-  | compatibleTwoPMap op1 op2
-  , Nothing <- Map.lookup k m
-  = lemmaInsertPendingTwice k vop vop' p
+  | Nothing <- Map.lookup k m
+  , Nothing <- Map.lookup k p
+  =    compatibleStateTwoPMap (applyTwoPMap x (TwoPMapApply k vop)) (TwoPMapApply k vop')
+  ==. compatibleStateTwoPMap (TwoPMap m1 p1 t1) (TwoPMapApply k vop')
+      ? lemmaInsertPendingTwice k vop vop' p
+      ? lemmaLookupInsert p k l0
+      ? lemmaLookupInsert p k l1
+      ? lawCompatibilityCommutativity vop' vop
+  ==. allCompatible (vop':l0)
+  ==. allCompatible (vop':[vop])
+  ==. compatible vop' vop
+  *** QED
+  | Nothing <- Map.lookup k m
+  , Just ops <- Map.lookup k p
+  =   compatibleStateTwoPMap (applyTwoPMap x (TwoPMapApply k vop)) (TwoPMapApply k vop')
+  ==. compatibleStateTwoPMap (TwoPMap m1 p1 t1) (TwoPMapApply k vop')
+      ? lemmaInsertPendingTwice k vop vop' p
+      ? lemmaLookupInsert p k l0
+      ? lemmaLookupInsert p k l1
+      ? lawCompatibilityCommutativity vop' vop
+      ? lemmaAllCompatibleInsert ops vop' vop
+  ==. allCompatible (vop':l0)
+  ==. allCompatible (vop':insertList vop ops)
+  ==. allCompatible' vop' (insertList vop ops)
+
+  *** QED
+  | otherwise = undefined
   where l0 = case Map.lookup k p of
                Nothing -> [vop]
                Just ops -> insertList vop ops
