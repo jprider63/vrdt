8aafc6b56a7a99850797cddca5b81c547b878368
Yiyun Liu <liuyiyun@terpmail.umd.edu>
2020-05-22 23:46:46 -0400

    finish LemmaAAEq

--- vrdt/src/VRDT/TwoPMap/LemmaAAEq.hs
+++ vrdt/src/VRDT/TwoPMap/LemmaAAEq.hs
@@ -20,7 +20,16 @@ import           VRDT.Class.Proof
 
 {-@ lemmaAllCompatibleInsert :: (Ord (Operation a), VRDT a) => ops:[Operation a] -> v0:Operation a -> v1:Operation a -> {(allCompatible' v0 ops && allCompatible' v1 ops && compatible v0 v1) => allCompatible' v0 (insertList v1 ops)} @-}
 lemmaAllCompatibleInsert :: (Ord (Operation a), VRDT a) => [Operation a] -> Operation a -> Operation a -> ()
-lemmaAllCompatibleInsert = undefined
+lemmaAllCompatibleInsert ops v0 v1
+  | not (allCompatible' v0 ops && allCompatible' v1 ops && compatible v0 v1) = ()
+lemmaAllCompatibleInsert [] v0 v1 = ()
+lemmaAllCompatibleInsert (op:ops) v0 v1
+  | v1 <= op
+  = ()
+  | otherwise
+  =   lemmaAllCompatibleInsert ops v0 v1
+    ? lemmaAllCompatibleInsert ops op v1
+    ? lawCompatibilityCommutativity op v1
 
 {-@ lawCommutativityAAEq :: (Ord k, Ord (Operation v), VRDT v) => x : TwoPMap k v -> k1:k -> vop1:Operation v -> vop2:Operation v -> {(compatibleTwoPMap (TwoPMapApply k1 vop1) (TwoPMapApply k1 vop2) && compatibleStateTwoPMap x (TwoPMapApply k1 vop1) && compatibleStateTwoPMap x (TwoPMapApply k1 vop2))  => ((applyTwoPMap (applyTwoPMap x (TwoPMapApply k1 vop1)) (TwoPMapApply k1 vop2) == applyTwoPMap (applyTwoPMap x (TwoPMapApply k1 vop2)) (TwoPMapApply k1 vop1)) && compatibleStateTwoPMap (applyTwoPMap x (TwoPMapApply k1 vop1)) (TwoPMapApply k1 vop2))} @-}
 lawCommutativityAAEq :: (Ord k, Ord (Operation v), VRDT v) => TwoPMap k v -> k -> Operation v -> Operation v -> ()
@@ -64,9 +73,7 @@ lawCommutativityAAEq x@(TwoPMap m p t) k vop vop'
   ==. allCompatible (vop':l0)
   ==. allCompatible (vop':insertList vop ops)
   ==. allCompatible' vop' (insertList vop ops)
-
   *** QED
-  | otherwise = undefined
   where l0 = case Map.lookup k p of
                Nothing -> [vop]
                Just ops -> insertList vop ops
@@ -76,10 +83,3 @@ lawCommutativityAAEq x@(TwoPMap m p t) k vop vop'
         op1 = TwoPMapApply k vop
         op2 = TwoPMapApply k vop'
         TwoPMap m1 p1 t1 = applyTwoPMap x op1
-
-
--- -- ok
-
-
--- -- ok
--- -- lawCommutativity x@(TwoPMap m p t) op1@(TwoPMapApply k vop) op2@(TwoPMapApply k' vop')
