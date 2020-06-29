da06ffa4c42cde8e98506f8f387040899786f292
Yiyun Liu <liuyiyun@terpmail.umd.edu>
2020-06-29 16:12:21 -0400

    commit .dir-locals.el

---
+++ .dir-locals.el
@@ -0,0 +1,7 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil
+  (dante-methods stack)
+  (dante-target . "vrdt")
+  (dante-load-flags . ("-UNotLiquid" "+c" "-fdiagnostics-color=never" "-fno-diagnostics-show-caret" "-Wwarn=missing-home-modules" "-ferror-spans" ))))
